61062589a8caf5c686026a2ecdeaa6c90f443449
Nithya Narayanan <n.nithya@pm.me>
2021-11-12 14:08:13 -0800

    Commit 9.

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -1,4 +1,38 @@
 describe('', () => {
         beforeEach(() => {
-            cy.visit('http://localhost:3000');
+            cy.visit('http://localhost:3000/pizza');
         })
+
+    const nameInput = () => cy.get('input[name = name]');
+    const oliveBox = () => cy.get ('input[name=olives]')
+    const caperBox = () => cy.get ('input[name=capers]')
+    const onionsBox = () => cy.get ('input[name=onions]')
+    const pepperBox = () => cy.get ('input[name=bellpeppers]')
+    const AddToOrderBtn = () => cy.get('button[type = submit]')
+
+    it('Make sure the elements show', () => {
+
+        nameInput().should('exist');
+        oliveBox().should('exist');
+        caperBox().should('exist');
+        onionsBox().should('exist');
+        pepperBox().should('exist');
+        AddToOrderBtn().should('exist');
+
+    })
+
+    it ('Inputs work?', () => {
+        nameInput().should('have.value', '')
+                    .type('text')
+                    .should('have.value', 'text');
+        
+        oliveBox().should('not.be.disabled')
+        caperBox().should('not.be.disabled')
+        onionsBox().should('not.be.disabled')
+        pepperBox().should('not.be.disabled')
+        AddToOrderBtn().should('not.be.disabled')
+                        
+    })
+
+
+ })
