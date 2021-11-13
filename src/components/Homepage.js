9d0b4256d94b87ce3d2f70d3616864339dc92d6c
Nithya Narayanan <n.nithya@pm.me>
2021-11-12 17:02:23 -0800

    done.

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import Navigation from "./Navigation";
-import "../App.css";
+import React from 'react'
+import {Link} from 'react-router-dom'
+import "../App.css"
 
 export default function Home(){
     return(
-        <Navigation />
-    );
-};
+                <div className="nav-container">
+                    <nav>
+                        <h1>Lambda Eats</h1>
+                        <Link to="/">Home </Link>
+                    </nav>
+                    <h2>Hungry?</h2>
+                    <nav>
+                    <Link id="order-pizza" to="/pizza">
+                    <h2>Start Here</h2>
+                    </Link>
+                    </nav>
+                </div>
+            );
